a40f36bad77afae540b55d942bb84bf4572016ae
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2015-07-04 19:20:03 +0000

    Startup now checks if user is not logged as root.

--- etc/profile.d/startup.sh
+++ etc/profile.d/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Loads the X11 environment from the ~/.xinitrc if the user login into the TTY1 and if a X environment is not already started.
 
-if [[ "$(tty)" == '/dev/tty1' ]]; then
+# if user is not a super user and is logged in tty1
+if [[ "$(id -u)" != "0" && "$(tty)" == '/dev/tty1' ]]; then
     [[ -z "$(pgrep xinit)" ]] && [[ -e "${HOME}/.xinitrc" ]] && exec bash ${HOME}/.xinitrc || echo "[ERROR] '${HOME}/.xinitrc' does not exist."
 fi
